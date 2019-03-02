aa61e9cfdf9d4e2f377770c959696061635eb8e4
Anna Szmyga <anna.szmyga@o2.pl>
2019-03-02 16:24:34 +0100

    Modyfy JS code and add error alert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var url = 'https://restcountries.eu/rest/v1/name/';
+var url = 'https://restcountries.eu/rest/v2/name/';
 var countriesList = document.getElementById('countries');
 
 
@@ -8,18 +8,24 @@ var countriesList = document.getElementById('countries');
 document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
+    document.querySelector('.alert').classList.remove('show');
     var countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
     fetch(url + countryName)
         .then(function(resp) {
             return resp.json();
         })
-        .then(showCountriesList);
+        .then(showCountriesList)
+        .catch(function(error) {
+            document.querySelector('.alert').classList.add('show');
+        });
 };
 
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item){
+        
+        // creating HTML elements
         var listElement = document.createElement('li');
         
         var headingWrapper = document.createElement('div');
@@ -28,23 +34,40 @@ function showCountriesList(resp) {
         var flagBox = document.createElement('div');
         flagBox.classList.add('flag-box');
         var flagImage = document.createElement('img');
+        flagImage.setAttribute('src', item.flag);
         flagBox.appendChild(flagImage);
 
         headingWrapper.appendChild(flagBox);
 
         var listElementHeading = document.createElement('h2');
         listElementHeading.innerText = item.name;
+        var headingSpan = document.createElement('span');
+        headingSpan.innerText = ' / ' + item.region +', ' + item.subregion;
+        listElementHeading.appendChild(headingSpan);
+        console.log(headingSpan);
         headingWrapper.appendChild(listElementHeading);
         listElement.appendChild(headingWrapper);
         
         var table = document.createElement('table');
+        
+        // getting data for table
         var countryInfo = {
             capital: ['Capital', item.capital],
             area: ['Land area', item.area + ' sq. km'],
             population: ['Population', item.population],
-            currencies: ['Currency', item.currencies], 
-            languages: ['Language(s)', item.languages]
+            currencies: ['Currency', item.currencies[0].code + ' (' + item.currencies[0].name + ')'], 
+            languages: ['Language(s)', item.languages[0].iso639_1 + ' (' + item.languages[0].name + ')']
         };
+        for (var i = 1; i < item.currencies.length; i++) {
+            countryInfo.currencies[1] += ', ' + item.currencies[i].code + ' (' + item.currencies[i].name + ')';
+        };
+        for (var i = 1; i < item.languages.length; i++) {
+            countryInfo.languages[1] += ', ' + item.languages[i].iso639_1 + ' (' + item.languages[i].name + ')';
+        };
+
+
+
+        // creating table rows
         for (var key in countryInfo) {
             var tableRow = document.createElement('tr');
             
@@ -60,14 +83,6 @@ function showCountriesList(resp) {
             table.appendChild(tableRow);
         }
         listElement.appendChild(table);
-
-        fetch('https://restcountries.eu/rest/v2/alpha/' + item.alpha3Code)
-            .then(function(flagResp) {
-                return flagResp.json();
-            })
-            .then(function(flagResp) {
-                flagImage.setAttribute('src', flagResp.flag);
-            });
 
         countriesList.appendChild(listElement);
     });
